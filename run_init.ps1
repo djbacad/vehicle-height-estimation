cfb49bc15631b79dd05ce26e4a3b1b81763a78e8
Dave Jammin Bacad <djbacad@gmail.com>
2025-02-23 17:06:42 +0800

    Update run_init.ps1

--- run_init.ps1
+++ run_init.ps1
@@ -12,25 +12,32 @@ function Write-EmojiMessage {
 
 Write-EmojiMessage "🚀 Starting initialization..." Cyan
 
-# 1. Add the git submodule
+# 1. Create Virtual Env
+Write-EmojiMessage "🥽 Creating virtual environment..." Yellow
+python -m venv venv_vhe
+venv_vhe/Scripts/activate
+Write-EmojiMessage "✅ Virtual Environment created successfully!" Green
+Write-Host ""
+
+# 2. Add the git submodule
 Write-EmojiMessage "📥 Adding git submodule..." Yellow
 git submodule add https://github.com/DepthAnything/Depth-Anything-V2 src/third_party
 Write-EmojiMessage "✅ Git submodule added successfully!" Green
 Write-Host ""
 
-# 2. Install pip requirements
+# 3. Install pip requirements
 Write-EmojiMessage "📦 Installing pip requirements..." Yellow
 pip install -r requirements.txt
 Write-EmojiMessage "✅ Pip requirements installed!" Green
 Write-Host ""
 
-# 3. Install PyTorch, torchvision, and torchaudio with CUDA 12.6 support
+# 4. Install PyTorch, torchvision, and torchaudio with CUDA 12.6 support
 Write-EmojiMessage "🛠 Installing PyTorch, torchvision, and torchaudio..." Yellow
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
 Write-EmojiMessage "✅ PyTorch packages installed!" Green
 Write-Host ""
 
-# 4. Install NVIDIA cuDNN for CUDA 12
+# 5. Install NVIDIA cuDNN for CUDA 12
 Write-EmojiMessage "🔧 Installing NVIDIA cuDNN..." Yellow
 py -m pip install nvidia-cudnn-cu12
 Write-EmojiMessage "✅ NVIDIA cuDNN installed!" Green
